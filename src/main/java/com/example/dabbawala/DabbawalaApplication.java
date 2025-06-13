f36ba634c536ae9ca25db8885891821d0c9341f3
Yashwant Gavali <ygavali@ptc.com>
2025-06-13 18:45:22 +0530

    initial commit

---
+++ src/main/java/com/example/dabbawala/DabbawalaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dabbawala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DabbawalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DabbawalaApplication.class, args);
+	}
+
+}
